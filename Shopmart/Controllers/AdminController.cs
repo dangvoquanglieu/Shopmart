2ab27a395ac9a1bba0ea0ce01b7af5efdadbc9f4
Lieu Dang <lieuhocgioisomot@gmail.com>
2021-03-19 16:36:15 +0700

    Bố sung thêm về giao diện Tích hợp shopping cart

--- Shopmart/Controllers/AdminController.cs
+++ Shopmart/Controllers/AdminController.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Shopmart.Data;
 using Shopmart.Models;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
--- Shopmart/Controllers/HomeController.cs
+++ Shopmart/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Shopmart.Data;
 using Shopmart.Models;
 using System;
 using System.Collections.Generic;
@@ -10,22 +11,38 @@ using System.Threading.Tasks;
 
 namespace Shopmart.Controllers
 {
+    
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private ApplicationDbContext db;
+        public HomeController(ApplicationDbContext db)
         {
-            _logger = logger;
+            this.db = db;
         }
 
         public IActionResult Index()
         {
+            var listProduct = db.Products.Select(s => new Product
+            {
+                ProductID = s.ProductID,
+                ProductName = s.ProductName,
+                UrlImage = s.UrlImage,
+                Price = s.Price,
+                Description = s.Description,
+                Quantity = s.Quantity,
+                Category = db.Categories.Where(c => c.CategoryID == s.CategoryID).FirstOrDefault()
+            }).ToList();
+            return View(listProduct);
+        }
+
+        public IActionResult Privacy()
+        {
             return View();
         }
 
-        [Authorize(Roles = "Admin")]
-        public IActionResult Privacy()
+        [Authorize(Roles = "User")]
+        public IActionResult Shopping()
         {
             return View();
         }
